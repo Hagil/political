d79769c84fd73ebfc5829af0d96e0849254ab220
Hagil Baik <hagilbaik@gmail.com>
2017-11-22 17:06:51 +0800

    Turnaucka's Law: 	The attention span of a computer is only as long as its 	electrical cord.

--- public/js/frontend_app.js
+++ public/js/frontend_app.js
@@ -1,1 +1,8 @@
 console.log('loaded frontend_app');
+
+var frontend_app = angular.module('presidents', []);
+frontend_app.controller('data', data_stuff);
+
+function do_data(){
+
+}
--- routes/main_routes.js
+++ routes/main_routes.js
@@ -1,9 +1,92 @@
 var router = require('express').Router();
+var PRESIDENTCLASS = require('../mongodb/mongoose_connection');
 module.exports = router;
 
 router.get('/', do_homepage);
 
-function do_homepage(req, res){
+function do_homepage(req, res) {
     console.log('doing homepage');
     res.render('index');
+}
+
+// routes for html controls eg dropdowns, radiosbuttons, checkboxes etc
+router.get('/api/v4/partydropdown', partydropdown);
+
+function partydropdown(req, res) {
+    console.log('getting unique party list');
+    PRESIDENTCLASS.find().distinct('party').then(function (party) {
+        console.log(party);
+        res.json(party);
+    })
+}
+//api
+
+router.get('/api/v4/read', do_read);
+router.post('/api/v4/create', do_create);
+router.put('/api/v4/update', do_update);
+router.delete('/api/v4/delete/:_id', do_delete);
+
+function do_read(req, res) {
+    console.log('reading all data');
+    PRESIDENTCLASS.find().then(function (results) {
+        console.log(results);
+        res.json(results);
+    });
+}
+
+function do_create(req, res) {
+    console.log('creating president');
+    console.log(req.body);
+
+    var data = {
+        number: req.body.number,
+        president: req.body.president,
+        birth_year: req.body.birth_year,
+        death_year: req.body.death_year,
+        took_office: req.body.took_office,
+        left_office: req.body.left_office,
+        party: req.body.party
+    }
+
+    var president = new PRESIDENTCLASS(data);
+    president.save().then(function (result) {
+        console.log(result);
+        res.json({
+            message: 'backend saved!'
+        });
+    });
+}
+
+function do_update(req, res) {
+    console.log('updating president');
+    console.log(req.body);
+
+    var update = {
+        $set: {
+            number: req.body.number,
+            president: req.body.president,
+            birth_year: req.body.birth_year,
+            death_year: req.body.death_year,
+            took_office: req.body.took_office,
+            left_office: req.body.left_office,
+            party: req.body.party
+        }
+    }
+    PRESIDENTCLASS.findByIdAndUpdate(req.body._id, update).then(function (result) {
+        console.log(result);
+        res.json({
+            message: 'backend updated!'
+        });
+    });
+}
+
+function do_delete(req, res) {
+    console.log('deleting president');
+    console.log(req.params);
+   PRESIDENTCLASS.findByIdAndRemove(req.params._id).then(function (result) {
+        console.log(result);
+        res.json({
+            message: 'deleted!'
+        });
+    });
 }
