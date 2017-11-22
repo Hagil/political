bae833d028cd901bd2886fe40189f8e9d269ac79
Hagil Baik <hagilbaik@gmail.com>
2017-11-22 17:35:54 +0800

    The world's great men have not commonly been great scholars, nor its great scholars great men. 		-- Oliver Wendell Holmes

--- public/js/frontend_app.js
+++ public/js/frontend_app.js
@@ -1,8 +1,69 @@
 console.log('loaded frontend_app');
 
 var frontend_app = angular.module('presidents', []);
-frontend_app.controller('data', data_stuff);
+frontend_app.controller('data', do_data);
 
-function do_data(){
+function do_data($scope, $http) {
+    console.log('inside dodata');
+    $scope.message = 'Welcome to US Politics';
+    $scope.read = function () {
+        console.log('reading data from backend');
+        $http.get('/api/v4/read').then(function (presidents) {
+            $scope.presidents = presidents.data;
+        });
+    }
+    $scope.read();
 
+    $scope.create = function () {
+        console.log('creating president');
+        var data = {
+            number: $scope.input_number,
+            president: $scope.input_president,
+            birth_year: $scope.input_birth_year,
+            death_year: $scope.input_death_year,
+            took_office: $scope.input_took_office,
+            left_office: $scope.input_left_office,
+            party: $scope.input_party
+        }
+        $http.post('/api/v4/create', data)
+            .then(function (result) {
+                console.log(result);
+                $scope.message = result.data.message;
+                $scope.read();
+            });
+    }
+
+    $scope.update = function (president) {
+        console.log('updating president');
+        var data = {
+            number: president.number,
+            president: president.president,
+            birth_year: president.birth_year,
+            death_year: president.death_year,
+            took_office: president.took_office,
+            left_office: president.left_office,
+            party: president.party
+        }
+        $http.put('/api/v4/update', data).then(function (result) {
+            console.log(result);
+            $scope.message = result.data.message;
+            $scope.read();
+        });
+    }
+    $scope.delete = function (president) {
+        console.log('deleting president');
+        $http.delete('/api/v4/delete/' + president._id).then(function (result) {
+            console.log(result);
+            $scope.message = result.data.message;
+            $scope.read();
+        });
+    }
+    $scope.dropdown = function () {
+        console.log('getting parties');
+        $http.get('/api/v4/partydropdown').then(function (party) {
+            console.log(party);
+            $scope.party = party.data;
+        });
+    }
+    $scope.dropdown();
 }
